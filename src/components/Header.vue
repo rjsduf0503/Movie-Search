aadb7a35bd45aa4b97c1c0b44a9534af1d72b051
류건열 <rjsduf0503@naver.com>
2022-12-10 21:24:19 +0900

    style: header animation 추가 및 검색 아이콘 변경

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,16 @@
 <template>
 	<header>
-		<div class="input-container">
-			<input v-model="title" type="text" placeholder="Please enter the title" />
-			<div @click="searchMovie">🔍</div>
-		</div>
+		<form class="input-container" @submit="onSubmit">
+			<input
+				ref="input"
+				v-model="title"
+				type="text"
+				placeholder="Please enter the title"
+			/>
+			<div @click="onSubmit">
+				<FontAwesomeIcon icon="fa-solid fa-search" />
+			</div>
+		</form>
 	</header>
 </template>
 
@@ -20,6 +27,11 @@
 			},
 		},
 		methods: {
+			onSubmit(e) {
+				e.preventDefault();
+				this.$refs.input.blur();
+				this.searchMovie();
+			},
 			searchMovie() {
 				const title = this.$store.state.keyword.title.trim();
 
@@ -53,19 +65,25 @@
 		min-width: 240px;
 		height: 3rem;
 		position: relative;
+		transition: all 300ms ease-out;
+		display: flex;
+		flex-direction: row;
+
+		&:focus-within {
+			transform: scale(1.2);
+		}
 
 		input {
-			width: calc(100% - 5rem);
+			width: calc(100% - 1rem);
 			min-width: 220px;
 			height: 3rem;
 			font-size: 1.4rem;
 			font-weight: bold;
 			border: 0;
-			border-radius: 5rem;
+			border-radius: 5rem 0 0 5rem;
 			outline: none;
 			padding-left: 1rem;
 			background-color: rgb(68, 68, 68);
-			padding-right: 4rem;
 			color: white;
 
 			&::placeholder {
@@ -74,11 +92,15 @@
 		}
 
 		div {
-			position: absolute;
-			font-size: 2rem;
-			right: 1rem;
-			top: 0.5rem;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size: 1.7rem;
 			cursor: pointer;
+			background-color: rgb(53, 53, 53);
+			height: 3rem;
+			width: 5rem;
+			border-radius: 0 5rem 5rem 0;
 		}
 	}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import '@/styles/normalize.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(faSearch);
+
 import { createApp } from 'vue';
 import App from '@/App';
 import store from '@/store';
 import router from '@/routes';
 
 const app = createApp(App);
+app.component('FontAwesomeIcon', FontAwesomeIcon);
 app.use(store);
 app.use(router);
 app.mount('#app');
